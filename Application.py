faaabbd3b4a5a4e55474a1571ee665473f0a1dd3
Jeremy Nagel <jeremymnagel@gmail.com>
2018-05-13 11:31:06 +1000

    Add purchase order

--- Application.py
+++ Application.py
@@ -4,6 +4,7 @@ import requests
 import logging
 import os
 import pdfkit
+import platform
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -103,7 +104,11 @@ def sales_confirmation():
         'page-width': '20in',
         'no-outline': None
     }
-    config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
+
+    if (platform.system() == 'Darwin'):
+        config = pdfkit.configuration()
+    else:
+        config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
     pdf = pdfkit.from_string(html, False, options=options, configuration=config)
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
@@ -111,6 +116,118 @@ def sales_confirmation():
     if sales_order_id is None:
         sales_order_id = "2999925000000387032"
     response.headers['Content-Disposition'] = "inline; filename=sales-order-confirmation-" + sales_order_id + ".pdf"
+    return response, 200
+
+
+@app.route('/purchase-order-html')
+def purchase_order_html():
+    purchase_order_id = request.args.get('purchase_order_id')
+    if purchase_order_id is None:
+        purchase_order_id = "2999925000000387032"
+
+    argument_json = json.dumps({'purchase_order_id': purchase_order_id})
+    payload = {'arguments': argument_json}
+
+    results = requests.post(
+        "https://crm.zoho.com/crm/v2/functions/data_for_purchase_order/actions/execute?auth_type=apikey&zapikey=1003.8f64ec64d9560c2c7e810f80fd21e49d.2add21fec0a719b739fa18725edab95b",
+        data=payload)
+    result_dict = json.loads(results.text)
+    output_json = result_dict['details']['output']
+    output_dict = json.loads(output_json)
+    purchase_order_details = output_dict["purchase_order_data"]
+    client_name = purchase_order_details["Client"]["name"] if "Client" in purchase_order_details and purchase_order_details[
+        "Client"] != None else ""
+    supplier_name = purchase_order_details["Supplier"]["name"] if "Supplier" in purchase_order_details and purchase_order_details[
+        "Supplier"] != None else ""
+    client_contact_name = purchase_order_details["Client_Contact"]["name"] if "Client_Contact" in purchase_order_details and \
+                                                                           purchase_order_details[
+                                                                               "Client_Contact"] != None else ""
+
+    product_dict = {}
+    for product in output_dict["product_data"]:
+        product_dict[product["id"]] = product
+
+    def process_line_item(line_item):
+        related_product = product_dict[line_item["Product"]["id"]]
+        line_item["Product"] = related_product
+        return line_item
+
+    purchase_order_details["PO_Line_Items"] = map(process_line_item, purchase_order_details["PO_Line_Items"])
+
+    strike_off_lab_dips = output_dict["strike_off_lab_dips"]
+    strike_off = None
+    lab_dip = None
+    for strike_off_lab_dip in strike_off_lab_dips:
+        if strike_off_lab_dip["Strike_Off_or_Lab_Dip"] == "Strike-Off":
+            strike_off = strike_off_lab_dip
+        else:
+            lab_dip = strike_off_lab_dip
+    shipments = output_dict["shipments"]
+    sample_shipment = None
+    shipping_sample_shipment = None
+    bulk_shipment = None
+    for shipment in shipments:
+        shipment_type = shipment["Shipment_Type"] if "Shipment_Type" in shipment else ""
+        if (shipment_type == "Bulk Shipment"):
+            bulk_shipment = shipment
+        elif (shipment_type == "Sample"):
+            sample_shipment = shipment
+        elif (shipment_type == "Shipping Sample"):
+            shipping_sample_shipment = shipment
+
+    shipping_sample_shipment_date = shipping_sample_shipment[
+        "Ex_mill_date"] if shipping_sample_shipment != None and "Ex_mill_date" in shipping_sample_shipment else "N/A"
+    sample_shipment_date = sample_shipment[
+        "Ex_mill_date"] if sample_shipment != None and "Ex_mill_date" in sample_shipment else "N/A"
+    bulk_shipment_date = bulk_shipment[
+        "Ex_mill_date"] if bulk_shipment != None and "Ex_mill_date" in bulk_shipment else "N/A"
+
+    data = {
+        "client_contact_name": client_contact_name,
+        "client_name": client_name,
+        "supplier_name": supplier_name,
+        "strike_off_lab_dips": strike_off_lab_dips,
+        "sample_shipment": sample_shipment,
+        "shipping_sample_shipment": shipping_sample_shipment,
+        "bulk_shipment": bulk_shipment,
+        "shipping_sample_shipment_date": shipping_sample_shipment_date,
+        "sample_shipment_date": sample_shipment_date,
+        "bulk_shipment_date": bulk_shipment_date,
+        "purchase_order_details": purchase_order_details,
+        "strike_off": strike_off,
+        "lab_dip": lab_dip
+    }
+
+
+    html = render_template('./purchase_order.html', title='Purchase Order', data=data)
+    return html
+
+@app.route('/purchase-order')
+def purchase_order():
+    html = purchase_order_html()
+    options = {
+        'page-size': 'A4',
+        'dpi': 240,
+        'margin-top': '0.15in',
+        'margin-right': '0.15in',
+        'margin-bottom': '0.15in',
+        'margin-left': '0.15in',
+        'encoding': "UTF-8",
+        'page-width': '20in',
+        'no-outline': None
+    }
+
+    if (platform.system() == 'Darwin'):
+        config = pdfkit.configuration()
+    else:
+        config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
+    pdf = pdfkit.from_string(html, False, options=options, configuration=config)
+    response = make_response(pdf)
+    response.headers['Content-Type'] = 'application/pdf'
+    purchase_order_id = request.args.get('purchase_order_id')
+    if purchase_order_id is None:
+        purchase_order_id = "2999925000000387032"
+    response.headers['Content-Disposition'] = "inline; filename=purchase-order-" + purchase_order_id + ".pdf"
     return response, 200
 
 port = int(os.getenv('PORT', 8084))
